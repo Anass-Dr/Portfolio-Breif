5ddb88f7161eee7a793cae4de5a95c07a7aea49a
Anass-Dr <anasdrissi77@gmail.com>
2024-04-17 11:04:47 +0100

    Wrapper component added

--- react/src/pages/Portfolio.jsx
+++ react/src/pages/Portfolio.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { SidebarContext } from "../context/SidebarContext";
+import { SidebarContextProvider } from "../context/SidebarContext";
 import Home from "../components/portfolio/Home";
 import InfoBar from "../components/portfolio/InfoBar";
 import MenuBar from "../components/portfolio/MenuBar";
@@ -8,20 +9,15 @@ import Projects from "../components/portfolio/Projects";
 import "./Portfolio.css";
 
 function Portfolio() {
-    const { currNav } = useContext(SidebarContext);
-    console.log(currNav);
-
     return (
         <>
             <div className="art-app">
                 <div className="art-mobile-top-bar"></div>
                 <div className="art-app-wrapper">
                     <div className="art-app-container">
-                        <InfoBar />
-                        {currNav === 0 && <Home />}
-                        {currNav === 1 && <Formations />}
-                        {currNav === 2 && <Projects />}
-                        <MenuBar />
+                        <SidebarContextProvider>
+                            <Wrapper />
+                        </SidebarContextProvider>
                     </div>
                 </div>
             </div>
@@ -30,3 +26,17 @@ function Portfolio() {
 }
 
 export default Portfolio;
+
+function Wrapper() {
+    const { currNav } = useContext(SidebarContext);
+
+    return (
+        <>
+            <InfoBar />
+            {currNav === 0 && <Home />}
+            {currNav === 1 && <Formations />}
+            {currNav === 2 && <Projects />}
+            <MenuBar />
+        </>
+    );
+}
